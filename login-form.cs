afe6752fc51cc74c479bb8bbe4ee4b371add3f84
Moo-CBT <stephenm@crossbrowsertesting.com>
2017-09-05 14:28:31 -0500

    Updated Caps

--- login-form.cs
+++ login-form.cs
@@ -30,9 +30,10 @@ namespace LoginFormTest
             var caps = new DesiredCapabilities();
             caps.SetCapability("name", "Login Form Example");
             caps.SetCapability("build", "1.0");
-            // request latest chrome, try ff-latest for firefox
-            caps.SetCapability("browser_api_name", "chrome-latest");
-            caps.SetCapability("os_api_name", "Win10");
+            // request latest chrome by default
+            // To specify version add caps.SetCapability("version", "desired version")
+            caps.SetCapability("browserName", "Chrome");        
+            caps.SetCapability("platform", "Windows 10");
             caps.SetCapability("screen_resolution", "1366x768");
             caps.SetCapability("record_video", "true");
             caps.SetCapability("record_network", "true");
